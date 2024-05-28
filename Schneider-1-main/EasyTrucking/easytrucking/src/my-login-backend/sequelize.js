57c8bc688f28d8d19044f4f9c8167458f1719cd0
adamani2406 <116831058+adamani2406@users.noreply.github.com>
2024-05-28 02:58:23 -0500

    Delete Schneider-1-main directory

--- Schneider-1-main/EasyTrucking/easytrucking/src/my-login-backend/sequelize.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite', // This will store your database in a file named database.sqlite
-});
-
-module.exports = sequelize;
